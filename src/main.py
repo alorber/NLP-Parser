eff6f6beb84639996bd3e1e09da8e8d81982978a
Andrew Lorber <andrewlorber@aol.com>
2021-04-10 22:57:34 -0400

    Imports grammar file and stores rules

--- src/main.py
+++ src/main.py
@@ -1,3 +1,29 @@
 # Andrew Lorber
 # NLP Project 2 - Parser
 
+# Imports grammar files and stores grammar rules
+def importGrammar():
+    # Gets grammar file
+    grammar_file_name = input("Please enter name of file containing CFG in CNF:\n")
+    grammar_file = open(grammar_file_name, 'r')
+
+    # Builds dictionary from grammar rules
+    # { LHS : [RHS,RHS,...] }
+    grammar_rules = {}
+    for rule in grammar_file.read().split("\n"):
+        if rule != "":  # Ignores empty lines
+            lhs, rhs = rule.split(" --> ")
+            if lhs in grammar_rules:
+                grammar_rules[lhs].append(rhs.split(" "))
+            else:
+                grammar_rules[lhs] = [rhs.split(" ")]
+
+    return grammar_rules
+
+
+
+# Main
+# ----
+if __name__ == "__main__":
+    grammar_rules = importGrammar()
+    print(grammar_rules)
