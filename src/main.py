06becbafbcd5d0dea3ebc8903b9b3643ab868d83
Andrew Lorber <andrewlorber@aol.com>
2021-04-11 02:59:59 -0400

    Prints Parses & Parse Trees

--- src/main.py
+++ src/main.py
@@ -3,13 +3,15 @@
 
 import string
 
+
 # Class to store information in each cell
 # Allows retrieval of possible parses
 class CellNode:
-    def __init__(self, lhs, rhs1, rhs2):
-        self.lhs = lhs
-        self.rhs1 = rhs1
-        self.rhs2 = rhs2
+    def __init__(self, lhs, rhs1=None, rhs2=None):
+        self.lhs = lhs  # Left-hand side
+        self.rhs1 = rhs1  # Right-hand side 1
+        self.rhs2 = rhs2  # Right-hand side 2
+
 
 # Imports grammar files and stores grammar rules
 def import_grammar():
@@ -19,62 +21,98 @@ def import_grammar():
     grammar_file = open(grammar_file_name, 'r')
 
     # Builds dictionary from grammar rules
-    grammar_rules = {}  # { LHS : [RHS,RHS,...] }
     reverse_grammar_rules = {}  # { RHS : [LHS,LHS,...] }
     for rule in grammar_file.read().split("\n"):
         if rule != "":  # Ignores empty lines
             lhs, rhs = rule.split(" --> ")
-            if lhs in grammar_rules:
-                grammar_rules[lhs].append(rhs.split())
-            else:
-                grammar_rules[lhs] = [rhs.split()]
+
             if rhs in reverse_grammar_rules:
                 reverse_grammar_rules[rhs].append(lhs)
             else:
                 reverse_grammar_rules[rhs] = [lhs]
 
-    return grammar_rules, reverse_grammar_rules
+    return reverse_grammar_rules
 
 
 # Parses sentence using CKY Algorithm
-def parse_sentence(sentence, reversed_grammar_rules):
+def parse_sentence(sentence, reverse_grammar_rules):
     # Splits sentence into tokens
     tokens = sentence.split()
 
-    cky_chart = [[]*(len(tokens)+1)]*(len(tokens)+1)
+    # Table for CKY Algorithm
+    cky_chart = [[[] for _ in range(len(tokens) + 1)] for _ in range(len(tokens) + 1)]
 
     # Following pseudocode from textbook
-    for j in range(1, len(tokens)+1):
-        curr_token = tokens[j-1]
+    for j in range(1, len(tokens) + 1):
+        curr_token = tokens[j - 1]
 
-        for lhs in reversed_grammar_rules[curr_token]:
-            cky_chart[j-1][j].append(CellNode(lhs, CellNode(curr_token)))
+        for lhs in reverse_grammar_rules.get(curr_token, []):
+            cky_chart[j - 1][j].append(CellNode(lhs, CellNode(curr_token)))
 
-        for i in range(j-2, -1, -1):
-            for k in range(i+1, j):
+        for i in range(j - 2, -1, -1):
+            for k in range(i + 1, j):
                 for b in cky_chart[i][k]:
                     for c in cky_chart[k][j]:
-                        for lhs in reversed_grammar_rules[" ".join([b.lhs, c.lhs])]:
+                        for lhs in reverse_grammar_rules.get(" ".join([b.lhs, c.lhs]), []):
                             cky_chart[i][j].append(CellNode(lhs, b, c))
 
     return cky_chart
 
 
+# Recursively prints parse
+def rec_print_parse(parse_node, print_tree, num_indents):
+    divider = ("\n" + "\t" * num_indents) if print_tree else " "
+
+    if num_indents > 0:
+        print(divider, end="")
+
+    if parse_node.rhs2 is None:
+        print("[" + parse_node.lhs + " " + parse_node.rhs1.lhs + "]", end="")
+    else:
+        print("[" + parse_node.lhs, end="")
+        rec_print_parse(parse_node.rhs1, print_tree, num_indents + 1)
+        rec_print_parse(parse_node.rhs2, print_tree, num_indents + 1)
+        if print_tree:
+            print(divider, end="")
+        print("]", end="")
+
+
+# Prints valid parses
+def print_parses(cky_chart):
+    # Determines whether to print parse tree
+    print_tree = True if input("Would you like to display the parse tree? (y/n)\n").lower() == "y" else False
+
+    for parse_num, cell in enumerate(list(filter(lambda rule: rule.lhs == "S", cky_chart[0][-1])), 1):
+        print(f"Valid parse #{parse_num}")
+        rec_print_parse(cell, False, 0)
+        print("\n")
+        if print_tree:
+            rec_print_parse(cell, True, 0)
+            print("\n")
+
+
 # Main
 # ----
 if __name__ == "__main__":
-    grammar_rules, reversed_grammar_rules = import_grammar()
-    print(grammar_rules,"\n")
-    print(reversed_grammar_rules,"\n")
+    reverse_grammar_rules = import_grammar()
 
     while True:
         # Reads in sentence
         sentence = input("Please enter a sentence:\n")
         # Removes punctuation and converts to lowercase
-        sentence = sentence.translate(str.maketrans('','',string.punctuation)).lower()
+        sentence = sentence.translate(str.maketrans('', '', string.punctuation)).lower()
 
         # Checks for quit
         if sentence == "quit":
             break
 
-        parse_sentence(sentence, reversed_grammar_rules)
+        cky_chart = parse_sentence(sentence, reverse_grammar_rules)
+
+        # Checks for valid parses
+        num_parses = len(list(filter(lambda rule: rule.lhs == "S", cky_chart[0][-1])))
+
+        if num_parses == 0:
+            print("NO VALID PARSES\n")
+        else:
+            print_parses(cky_chart)
+            print(f"Number of valid parses: {num_parses}\n")
